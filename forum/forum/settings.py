8e0c16edc85c3d4fadb6cbe3c94146d78a5b3901
Arsen Strus <116881133+ArsenStrus@users.noreply.github.com>
2025-05-13 20:33:03 +0000

    Fixed secret_key in settings.py

--- forum/forum/settings.py
+++ forum/forum/settings.py
@@ -19,8 +19,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure--fa8pttjjjbhp!1a*igo+)ym#&lfb+qzv@#u@k&xw^e@*14c^+"
+import os  
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", "fallback-key-for-dev-only")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
