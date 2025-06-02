e1b11ccbc8c471c82a4ed125dc2b455f5ea8271b
Hlib Sokolovskyi <sglibr2004@gmail.com>
2025-06-03 00:15:11 +0200

    commented out wsgi_application in settings

--- forum/forum/settings.py
+++ forum/forum/settings.py
@@ -79,7 +79,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = "forum.wsgi.application"
+# WSGI_APPLICATION = "forum.wsgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
