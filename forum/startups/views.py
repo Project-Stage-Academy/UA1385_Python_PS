98168287c83ada0d9f16d17255c12635cc87ff45
AndrewTT12 <andriy551071@gmail.com>
2025-06-11 14:14:55 +0300

    Made sub/unsub function

--- forum/startups/views.py
+++ forum/startups/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from rest_framework import viewsets
-from .models import StartupProfile
+from .models import StartupProfile, Subscription
+from rest_framework.decorators import action
+from rest_framework.response import Response
 from .serializers import StartupProfileSerializer
 from rest_framework import filters
 from rest_framework.permissions import IsAuthenticated
@@ -12,3 +14,28 @@ class StartupProfileViewSet(viewsets.ModelViewSet):
     filter_backends = [filters.SearchFilter, filters.OrderingFilter]
     search_fields = ['title', 'industry', 'description', 'address']
     ordering_fields = ['title', 'industry']
+    
+    @action(detail=True, methods=['post'], permission_classes=[IsAuthenticated])
+    def subscribe(self, request, pk=None):
+        startup = self.get_object()
+        subscription, created = Subscription.objects.get_or_create(
+            investor=request.user,
+            startup=startup
+        )
+        if created:
+            return Response({"status": "subscribed"}, status=201)
+        return Response({"status": "already subscribed"}, status=200)
+
+    @action(detail=True, methods=['post'], permission_classes=[IsAuthenticated])
+    def unsubscribe(self, request, pk=None):
+        startup = self.get_object()
+        subscription = Subscription.objects.filter(
+            investor=request.user,
+            startup=startup
+        )
+
+        if subscription:
+            subscription.delete()
+            return Response({"status": "unsubscribed"}, status=200)
+        else:
+            return Response({"status": "not subscribed"}, status=400)
