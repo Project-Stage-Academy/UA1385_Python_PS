63934067760d42e68f553048f5da8a9d64a6aece
IrinaKrivoruchko <sokurira@gmail.com>
2025-06-19 18:13:56 +0300

    fix settings

--- forum/forum/settings.py
+++ forum/forum/settings.py
@@ -91,6 +91,7 @@ DATABASES = {
         "NAME": os.getenv("DB_NAME", "postgres"),
         "USER": os.getenv("DB_USER", "postgres"),
         "PASSWORD": os.getenv("DB_PASSWORD", "postgres"),
+        "HOST": os.getenv("DB_HOST", "db"),
         "PORT": os.getenv("DB_PORT", "5432"),
     }
 }
